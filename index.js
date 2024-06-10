c5dfd3c8b05a987798ea79fe243013cde38bc1b8
Alfan Andika <alfanandika65@gmail.com>
2024-06-10 11:00:03 +0700

    Perubahan message saat presentasi MBKM

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.status(200).send({
-        message : "Halo, ini test untuk laporan akhir kampus FTTE",
+        message : "Halo, ini test untuk laporan akhir presentasi MBKM tanggal 10 Juni 2024",
         status : "Success"
     })
 });
